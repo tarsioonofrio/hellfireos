dad4710a454a1ffacbcccce1eed5bb9b06a50235
tarsioonofrio <5504075+tarsioonofrio@users.noreply.github.com>
2020-06-13 21:03:42 -0300

    Home work is finished

--- usr-ex/noc_ex/noc_ex.c
+++ usr-ex/noc_ex/noc_ex.c
@@ -5,6 +5,7 @@
 #define EDGE_TARGET 1
 #define EDGE_WEIGHT 2
 #define EDGE_SIZE 17
+#define CPU_SIZE 11
 
 
 int arr_task_edge [EDGE_SIZE][3] = {
@@ -27,70 +28,60 @@ int arr_task_edge [EDGE_SIZE][3] = {
         {10, 11, 440}
 };
 
-uint8_t arr_task_node [11] = {0, 2, 1, 1, 1, 3, 3, 2, 4, 4, 5};
-
-
-typedef struct{
-    uint8_t size;
-    uint8_t *task;
-} struct_node;
+uint8_t arr_task_node [CPU_SIZE] = {0, 2, 1, 1, 1, 3, 3, 2, 4, 4, 5};
 
 typedef struct{
-    uint8_t target_node; //target_node
-    uint8_t target_task;
+    uint8_t cpu; //target_node
+    uint8_t task;
     uint8_t size;
-    int8_t *buf;
 } struct_task;
 
 
-void sender(void) {
+_Noreturn void sender(void) {
     int32_t i;
-    uint8_t n;
+    uint8_t n, t = 0;
     uint32_t crc;
-//    int8_t buf[500];
+    int8_t buf[1500];
     int16_t val;
-    int task_num=0;
+    int target_num=0;
 
     uint8_t task_id = strtol(hf_selfname(), (char **)NULL, 10);
-    printf("task_id %d, arr_task %d, hf_cpuid %d\n", task_id, arr_task_node[task_id - 1], hf_cpuid());
+//    printf("task_id %d, arr_task %d, hf_cpuid %d\n", task_id, arr_task_node[task_id - 1], hf_cpuid());
 
-    for (n = 0; n < EDGE_SIZE; n++) {
+    for (n = 0; n < EDGE_SIZE; n++)
         if (task_id == arr_task_edge[n][EDGE_SOURCE])
-            task_num++;
-    }
+            target_num++;
 
-    struct_task task_send[task_num];
+    struct_task arr_target[target_num];
 
     for (n = 0; n < EDGE_SIZE; n++) {
         if (task_id == arr_task_edge[n][EDGE_SOURCE]){
-            printf("%d %d %d %d\n", arr_task_edge[n][EDGE_SOURCE], arr_task_edge[n][EDGE_TARGET],
-                    arr_task_edge[n][EDGE_WEIGHT], arr_task_node[arr_task_edge[n][EDGE_TARGET] -1]);
+//            printf("%d %d %d %d\n", arr_task_edge[n][EDGE_SOURCE], arr_task_edge[n][EDGE_TARGET],
+//                    arr_task_edge[n][EDGE_WEIGHT], arr_task_node[arr_task_edge[n][EDGE_TARGET] -1]);
 
-            task_send[n].target_task = arr_task_edge[n][EDGE_TARGET];
-            task_send[n].target_node = arr_task_node[arr_task_edge[n][EDGE_TARGET] -1];
-            task_send[n].size = arr_task_edge[n][EDGE_WEIGHT];
-            task_send[n].buf = (int8_t *)malloc(task_send[n].size*sizeof(int8_t));
+            arr_target[t].task = arr_task_edge[n][EDGE_TARGET];
+            arr_target[t].cpu  = arr_task_node[arr_task_edge[n][EDGE_TARGET] - 1];
+            arr_target[t].size = arr_task_edge[n][EDGE_WEIGHT];
+//            arr_target[t].buf = (int8_t *)malloc(arr_target[n].size * sizeof(int8_t));
+            t++;
         }
     }
 
-    if (hf_comm_create(hf_selfid(), 1000, 0)){
+    if (hf_comm_create(hf_selfid(), 1000 + task_id, 0)){
         panic(0xff);
     }
-
     delay_ms(50);
 
-
     srand(hf_cpuid());
-
     // generate a unique channel number for this CPU
     while (1) {
-        for (n = 0; n < task_num; n++) {
-            for (i = 0; i < task_send[n].size - 4; i++) {
-                task_send[n].buf[i] = random() % 255;
+        for (n = 0; n < target_num; n++) {
+            for (i = 0; i < arr_target[n].size - 4; i++) {
+                buf[i] = random() % 255;
             }
-            crc = hf_crc32(task_send[n].buf, task_send[n].size - 4);
-            memcpy(task_send[n].buf + task_send[n].size - 4, &crc, 4);
-            val = hf_send(task_send[n].target_node, 5000 + task_send[n].target_task, task_send[n].buf, task_send[n].size, 0); // task_send[n].target_task);
+            crc = hf_crc32(buf, arr_target[n].size - 4);
+            memcpy(buf + arr_target[n].size - 4, &crc, 4);
+            val = hf_send(arr_target[n].cpu, 5000 + arr_target[n].task, buf, arr_target[n].size, task_id); // task_send[n].target_task);
             if (val)
                 printf("hf_send(): error %d\n", val);
 
@@ -102,34 +93,32 @@ void sender(void) {
 _Noreturn void receiver(void)
 {
 	int8_t buf[1500];
-	uint16_t source_cpu, source_port, source_size, source_channel;
+	uint16_t source_cpu, source_port, source_size;
 	int16_t val;
 	uint32_t crc;
-	int32_t i;
+	int32_t source_channel;
 
     uint8_t task_id = strtol(hf_selfname(), (char **)NULL, 10);
-    printf("task_id %d, arr_task %d, hf_cpuid %d\n", task_id, arr_task_node[task_id - 1], hf_cpuid());
-    source_channel = arr_task_node[task_id - 1];
+//    printf("task_id %d, arr_task %d, hf_cpuid %d\n", task_id, arr_task_node[task_id - 1], hf_cpuid());
 
 	if (hf_comm_create(hf_selfid(), 5000 + task_id, 0))
 		panic(0xff);
 	
 	while (1){
-		i = hf_recvprobe();
-		if (i >= 0) {
-			val = hf_recv(&source_cpu, &source_port, buf, &source_size, 0);//source_channel);
-			if (val){
-				printf("hf_recv(): error %d\n", val);
-			} else {
-				memcpy(&crc, buf + source_size - 4, 4);
-				printf("cpu %d, port %d, channel %d, size %d, crc %08x [free queue: %d]", source_cpu, source_port, i, source_size, crc, hf_queue_count(pktdrv_queue));
-				if (hf_crc32(buf, source_size - 4) == crc)
-					printf(" (CRC32 pass)\n");
-				else
-					printf(" (CRC32 fail)\n");
-			}
-		}
-	}
+        source_channel = hf_recvprobe();
+        if (source_channel < 0) continue;
+        val = hf_recv(&source_cpu, &source_port, buf, &source_size, source_channel);
+        if (val){
+            printf("hf_recv(): error %d\n", val);
+        } else {
+            memcpy(&crc, buf + source_size - 4, 4);
+            printf("cpu %d, port %d, channel %d, size %d, crc %08x [free queue: %d]", source_cpu, source_port, source_channel, source_size, crc, hf_queue_count(pktdrv_queue));
+            if (hf_crc32(buf, source_size - 4) == crc)
+                printf(" (CRC32 pass)\n");
+            else
+                printf(" (CRC32 fail)\n");
+        }
+    }
 }
 
 void app_main(void)
@@ -138,10 +127,28 @@ void app_main(void)
         case 0:
             hf_spawn(sender, 0, 0, 0, "1", 4096);
         case 1:
+            hf_spawn(sender, 0, 0, 0, "4", 4096);
+            hf_spawn(sender, 0, 0, 0, "5", 4096);
+            hf_spawn(sender, 0, 0, 0, "3", 4096);
             hf_spawn(receiver, 0, 0, 0, "4", 4096);
             hf_spawn(receiver, 0, 0, 0, "5", 4096);
             hf_spawn(receiver, 0, 0, 0, "3", 4096);
-        default:
-            printf("\n *** hf_cpuid %d *** \n", hf_cpuid());
+        case 2:
+            hf_spawn(sender, 0, 0, 0, "2", 4096);
+            hf_spawn(sender, 0, 0, 0, "8", 4096);
+            hf_spawn(receiver, 0, 0, 0, "2", 4096);
+            hf_spawn(receiver, 0, 0, 0, "8", 4096);
+        case 3:
+            hf_spawn(sender, 0, 0, 0, "6", 4096);
+            hf_spawn(sender, 0, 0, 0, "7", 4096);
+            hf_spawn(receiver, 0, 0, 0, "6", 4096);
+            hf_spawn(receiver, 0, 0, 0, "7", 4096);
+        case 4:
+            hf_spawn(sender, 0, 0, 0, "9", 4096);
+            hf_spawn(sender, 0, 0, 0, "10", 4096);
+            hf_spawn(receiver, 0, 0, 0, "9", 4096);
+            hf_spawn(receiver, 0, 0, 0, "10", 4096);
+        case 5:
+            hf_spawn(receiver, 0, 0, 0, "11", 4096);
     }
 }
